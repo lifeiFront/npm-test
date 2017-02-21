0e89cc321e5f5c4f1086949551dbe36a470e3004
lifeiFront <lifei971102991@163.com>
2017-02-21 17:16:18 +0800

    Add files via upload

---
+++ rc.js
@@ -0,0 +1,18 @@
+/**
+ * @file: rc.js
+ * @author: lifei
+ * @date: 20170221
+ * @description: rc.js npm包发布练习
+ *
+ * @loglevel
+ * default: warn
+ * values: silent, error, warn, http, info, verbose, silly
+ */
+
+
+
+module.exports = {
+    registry: 'https://registry.npm.taobao.org/',
+    disturl: 'http://npm.taobao.org/mirrors/node/',
+    loglevel: 'http'
+}
